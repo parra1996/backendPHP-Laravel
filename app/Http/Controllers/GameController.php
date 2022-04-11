07a98fc017526992f1400fb842e345a3d28fbaef
“juanP1996” <parrajp1996@gmail.com>
2022-04-11 19:39:15 +0200

    feat: agregada la funcion de traer todos los games

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
-    //
+    public function gamesAll(){
+        try {
+
+            $games = Game::all();
+            return $games;
+
+        } catch (QueryException $error) {
+
+            $codigoError = $error->errorInfo[1];
+            if($codigoError){
+                return "Error $codigoError";
+            }
+
+        }
+    }
 }
