10e42eeec4937f66b8209d153196949b5caf63b7
“juanP1996” <parrajp1996@gmail.com>
2022-04-11 15:06:51 +0200

    feat: conexion a la bbdd realizada

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -11,5 +13,30 @@ class UserController extends Controller
         // return response()->json(['message' => 'Hello World!']);7
 
         return "hello world";
+    }
+
+    public function register(Request $request)
+    {
+        {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:6',
+            ]);
+    
+            if($validator->fails()){
+                return response()->json($validator->errors()->toJson(),400);
+            }
+    
+            $user = User::create([
+                'name' => $request->get('name'),
+                'email' => $request->get('email'),
+                'password' => bcrypt($request->password)
+            ]);
+    
+            // $token = JWTAuth::fromUser($user);
+    
+            return response()->json(compact('user','token'),201);
+        }
     }
 }
