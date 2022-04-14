115306658be818ea7d677310f604d4d230bdd3c5
“juanP1996” <parrajp1996@gmail.com>
2022-04-14 17:49:22 +0200

    feat: funcion de leer perfil funcional

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,93 +11,89 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 class AuthController extends Controller
 {
     public function userRegister(Request $request)
-{
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|string|max:255',
-        'email' => 'required|string|email|max:255|unique:users',
-        'password' => 'required|string|min:6',
-    ]);
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
 
-    if($validator->fails()){
-        return response()->json($validator->errors()->toJson(),400);
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user', 'token'), 201);
     }
+    //Login usuario
 
-    $user = User::create([
-        'name' => $request->get('name'),
-        'email' => $request->get('email'),
-        'password' => bcrypt($request->password)
-    ]);
+    public function login(Request $request)
+    {
 
-    $token = JWTAuth::fromUser($user);
 
-    return response()->json(compact('user','token'),201);
-}
-//Login usuario
-// public function userLogin(Request $request)
-public function login(Request $request)
-{
+        $input = $request->only('email', 'password');
+        $jwt_token = null;
 
-   
-    $input = $request->only('email', 'password');
-    $jwt_token = null;
+        if (!$jwt_token = JWTAuth::attempt($input)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
 
-    if (!$jwt_token = JWTAuth::attempt($input)) {
-        return response()->json([
-            'success' => false,
-            'message' => 'Invalid Email or Password',
-        ], Response::HTTP_UNAUTHORIZED);
-    }
-
-    return response()->json([
-        'success' => true,
-        'token' => $jwt_token,
-    ]);
-    // $input = $request->only('email', 'password');
-    // $jwt_token = null;
-
-    // if (!$jwt_token = JWTAuth::attempt($input)) {
-    //     return response()->json([
-    //         'success' => false,
-    //         'message' => 'Invalid Email or Password',
-    //     ], Response::HTTP_UNAUTHORIZED);
-    // }
-
-    // return response()->json([
-    //     'success' => true,
-    //     'token' => $jwt_token,
-    // ]);
-}
-
-//Leer perfil
-public function profile()
-{
-    
-    return response()->json(auth()->user());;
-}
-
- 
-//Logout
-public function userLogout(Request $request)
-{
-    $this->validate($request, [
-        'token' => 'required'
-    ]);
-
-    try {
-        JWTAuth::invalidate($request->token);
         return response()->json([
             'success' => true,
-            'message' => 'User logged out successfully'
+            'token' => $jwt_token,
         ]);
-    } catch (\Exception $exception) {
-        return response()->json([
-            'success' => false,
-            'message' => 'Sorry, the user cannot be logged out'
-        ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        // $input = $request->only('email', 'password');
+        // $jwt_token = null;
+
+        // if (!$jwt_token = JWTAuth::attempt($input)) {
+        //     return response()->json([
+        //         'success' => false,
+        //         'message' => 'Invalid Email or Password',
+        //     ], Response::HTTP_UNAUTHORIZED);
+        // }
+
+        // return response()->json([
+        //     'success' => true,
+        //     'token' => $jwt_token,
+        // ]);
     }
-}
+
+    //Leer perfil
+    public function profile()
+    {
+        return response()->json(auth()->user());;
+    }
 
 
+    //Logout
+    public function userLogout(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
 
+        try {
+            JWTAuth::invalidate($request->token);
 
+            return response()->json([
+                'success' => true,
+                'message' => 'User logged out successfully'
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, the user cannot be logged out'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -30,26 +30,25 @@ Route::post('/register', [AuthController::class, "userRegister"]);
 //L0guear usuario
 Route::post('/login', [AuthController::class, "login"]);
 //Logout
-Route::post('/logout', [AuthController::class, "userLogout"]);
 
 Route::group([
     'middleware' => 'jwt.auth'
 ], function () {
-//Endpoints de usuarios
+    //Endpoints de usuarios
 
-//Leer perfil //lo meto aqui proque necesita jwt
-Route::get('/profile', [AuthController::class, 'profile']);
-//Leer todos los usuario
-Route::get('/users', [UserController::class, 'allUsers']);
-//Leer usuario por id
-Route::get('/users/{id}', [UserController::class, 'userByID']);
-//Leer usuario por nombre
-Route::get('/users/name/{name}', [UserController::class, 'userByName']);
-//Actualizar usuario por id
-Route::put('/users/{id}', [UserController::class, 'updateUser']);
-//Eliminar usuario por id
-Route::delete('/users/{id}', [UserController::class, 'deleteUser']);
-
+    Route::post('/logout', [AuthController::class, "userLogout"]);
+    //Leer perfil //lo meto aqui proque necesita jwt
+    Route::get('/profile', [AuthController::class, 'profile']);
+    //Leer todos los usuario
+    Route::get('/users', [UserController::class, 'allUsers']);
+    //Leer usuario por id
+    Route::get('/users/{id}', [UserController::class, 'userByID']);
+    //Leer usuario por nombre
+    Route::get('/users/name/{name}', [UserController::class, 'userByName']);
+    //Actualizar usuario por id
+    Route::put('/users/{id}', [UserController::class, 'updateUser']);
+    //Eliminar usuario por id
+    Route::delete('/users/{id}', [UserController::class, 'deleteUser']);
 });
 
 // users routes
@@ -63,18 +62,18 @@ Route::group([
     'middleware' => 'jwt.auth'
 ], function () {
 
-//leer todos los mensajes
-Route::get('/messages', [MessageController::class, 'allMessages']);
-//Crear nuevo mensaje
-Route::post('/messages', [MessageController::class, 'newMessage']);
-//Leer mensaje por id
-Route::get('/messages/{id}', [MessageController::class, 'messageByID']);
-//Actualizar mensaje por id
-Route::put('/messages/{id}', [MessageController::class, 'updateMessage']);
-//borrar mensaje
-Route::delete('/messages/{id}', [MessageController::class, 'deleteMessage']);
-//Leer mensajes por id party
-Route::post('/message/party/{id}', [MessageController::class, "messagesByPartyID"]);
+    //leer todos los mensajes
+    Route::get('/messages', [MessageController::class, 'allMessages']);
+    //Crear nuevo mensaje
+    Route::post('/messages', [MessageController::class, 'newMessage']);
+    //Leer mensaje por id
+    Route::get('/messages/{id}', [MessageController::class, 'messageByID']);
+    //Actualizar mensaje por id
+    Route::put('/messages/{id}', [MessageController::class, 'updateMessage']);
+    //borrar mensaje
+    Route::delete('/messages/{id}', [MessageController::class, 'deleteMessage']);
+    //Leer mensajes por id party
+    Route::post('/message/party/{id}', [MessageController::class, "messagesByPartyID"]);
 });
 
 //games routes 
@@ -84,15 +83,15 @@ Route::group([
     'middleware' => 'jwt.auth'
 ], function () {
 
-Route::get('/games', [GameController::class, "gamesAll"]);
-//Juego por id
-Route::post('/games', [GameController::class, "gamesAdd"]);
-//Nuevo juego
-Route::post('/games/{id}', [GameController::class, "gameByID"]);
-//Editar juego
-Route::put('/games/{id}', [GameController::class, "updateGame"]);
-//Borrado de juego
-Route::delete('/games/{id}', [GameController::class, "deleteGame"]);
+    Route::get('/games', [GameController::class, "gamesAll"]);
+    //Juego por id
+    Route::post('/games', [GameController::class, "gamesAdd"]);
+    //Nuevo juego
+    Route::post('/games/{id}', [GameController::class, "gameByID"]);
+    //Editar juego
+    Route::put('/games/{id}', [GameController::class, "updateGame"]);
+    //Borrado de juego
+    Route::delete('/games/{id}', [GameController::class, "deleteGame"]);
 });
 
 
@@ -103,17 +102,16 @@ Route::group([
     'middleware' => 'jwt.auth'
 ], function () {
 
-//Listar parties
-Route::get('/parties', [PartyController::class, "partiesAll"]);
-//Nueva party
-Route::post('/parties', [PartyController::class, "newParty"]);
-//Party por id de game
-Route::post('/parties/{id}', [PartyController::class, "partyBygame_id"]);
-//Actualizar party por id
-Route::post('/parties/{id}', [PartyController::class, "updateParty"]);
-//Modificar party
-Route::put('/parties/{id}', [PartyController::class, "deleteParty"]);
-//Borrar party
-Route::delete('/parties/game/{id}', [PartyController::class, "partiesBygame_id"]);
+    //Listar parties
+    Route::get('/parties', [PartyController::class, "partiesAll"]);
+    //Nueva party
+    Route::post('/parties', [PartyController::class, "newParty"]);
+    //Party por id de game
+    Route::post('/parties/{id}', [PartyController::class, "partyBygame_id"]);
+    //Actualizar party por id
+    Route::post('/parties/{id}', [PartyController::class, "updateParty"]);
+    //Modificar party
+    Route::put('/parties/{id}', [PartyController::class, "deleteParty"]);
+    //Borrar party
+    Route::delete('/parties/game/{id}', [PartyController::class, "partiesBygame_id"]);
 });
-
